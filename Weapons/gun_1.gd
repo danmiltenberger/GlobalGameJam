a0afc07f852c1e1694c4338e1b0a9aa9dfeab9bc
Momin Khan <foolmoron@gmail.com>
2024-01-27 12:14:34 -0500

    Gun shooting

--- Weapons/gun_1.gd
+++ Weapons/gun_1.gd
@@ -2,30 +2,32 @@ extends Node2D
 
 # when the player presses shoot, send a word in that direction
 
-var cooldown_sec_left: float
-var can_shoot: bool = true
-@onready var timer = Timer.new()
+@export var bullet_scn: PackedScene
+
+@export var cooldown_base_sec: float = 0.2
+var cooldown_remaining_sec: float = 0.0
+
+@onready var marker: Marker2D = get_node("InstancePoint")
 
 func _ready() -> void:
 	print_debug("gun 1 ready")
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("shoot"):
-		print_debug("shoot action pressed!")
-		shoot("word")
+	# point to mouse
+	var mouse_world_pos := get_global_mouse_position()
+	var gun_to_mouse := mouse_world_pos - global_position
+	var angle_to_mouse := gun_to_mouse.angle()
+	rotation = angle_to_mouse
 
+	# shoot automatically
+	cooldown_remaining_sec -= delta
+	if cooldown_remaining_sec <= 0.0:
+		if Input.is_action_pressed("shoot"):
+			shoot()
 
-func shoot(bullet_str: String):
-	var bullet  = load("res://Projectiles/" + bullet_str + ".tscn")
-	bullet.instantiate() 
-	
-
-func cooldown_timer(seconds):
-	timer.wait_time = seconds
-	timer.one_shot = true
-	timer.start()
-	cooldown_sec_left = timer.time_left
-	
-	
-func cooldown_timer_timeout():
-	can_shoot = true
+func shoot():
+	cooldown_remaining_sec = cooldown_base_sec
+	var bullet := bullet_scn.instantiate()
+	get_tree().get_current_scene().add_child(bullet)
+	bullet.global_position = marker.global_position
+	bullet.rotation = rotation
