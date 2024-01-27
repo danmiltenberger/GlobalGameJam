154b1e789a2844df412862b5ae7bdec483b0b74c
Momin Khan <foolmoron@gmail.com>
2024-01-27 18:25:53 -0500

    test lines

--- Custom Components/LabelResizer.gd
+++ Custom Components/LabelResizer.gd
@@ -4,7 +4,7 @@ class_name LabelResizer extends Node
 @onready var collision: CollisionShape2D = get_parent().get_node("CollisionShape2D")
 
 func set_text(text: String):
-    label.text = "  " + text.trim_prefix(" ").trim_suffix(" ") + "  "
+    label.set_text("  " + text.trim_prefix(" ").trim_suffix(" ") + "  ")
 
 func _process(_delta):
     collision.shape.extents = Vector2(label.size.x, label.size.y)
