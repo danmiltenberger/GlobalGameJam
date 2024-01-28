38c1936cbd13f56b56673a1aceab4b4487bf8079
Dan Miltenberger <dangarrou@gmail.com>
2024-01-28 17:17:08 -0500

    added statistics to ending

--- LevelControl/ending_menu.gd
+++ LevelControl/ending_menu.gd
@@ -1,10 +1,26 @@
 extends Node2D
 
+func _ready() -> void:
+	show_ending_statistics()
 
+func show_ending_statistics():
+	var jokes = Globals.jokes_landed
+	var damage = Globals.total_damage_taken
+	var healing = Globals.total_healing_taken
+	printt(jokes, damage, healing)
+	$GridContainer/JokeFloat.text = str(jokes)
+	$GridContainer/DamageFloat.text = str(damage)
+	$GridContainer/HealingFloat.text = str(healing)
+	
+func clear_ending_statistics():
+	Globals.jokes_landed = 0
+	Globals.total_damage_taken = 0
+	Globals.total_healing_taken = 0
 
 func _on_replay_button_down() -> void:
 	var index = Globals.current_level
 	Scn.change("level_"+str(index))
+	clear_ending_statistics()
 
 
 
